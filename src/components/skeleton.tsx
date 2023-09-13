c22f13bbd5dd3ccf305629976ff2005a3800aa03
dxu <xudavidb@gmail.com>
2023-09-12 18:36:19 -0700

    update skeleton

--- src/components/skeleton.tsx
+++ src/components/skeleton.tsx
@@ -1,4 +1,7 @@
-import { cn } from "@/utils/cn"
+"use client";
+
+import { cn } from "@/utils/cn";
+import * as React from "react";
 
 function Skeleton({
   className,
@@ -9,7 +12,7 @@ function Skeleton({
       className={cn("animate-pulse rounded-md bg-muted", className)}
       {...props}
     />
-  )
+  );
 }
 
-export { Skeleton }
+export { Skeleton };
