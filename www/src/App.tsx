b9beab7f81e7299df736f2678a6bbf421fef81e2
dxu <xudavidb@gmail.com>
2023-09-12 19:31:45 -0700

    temp

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -1,4 +1,4 @@
-import { ScrollArea } from "wobbui";
+import { ScrollArea, Toaster } from "wobbui";
 
 import { useState } from "react";
 
@@ -10,12 +10,13 @@ function App() {
   const [selected, setSelected] = useState<ComponentType>("Accordion");
   return (
     <div className="flex w-screen h-screen">
-      <ScrollArea>
-        <div className="flex flex-col  px-4 py-4 text-lg divide-y">
+      <Toaster />
+      <ScrollArea className="flex-shrink-0 border-r border-solid">
+        <div className="flex flex-col py-4 text-lg divide-y">
           {Object.entries(components).map(([key]) => {
             return (
               <a
-                className="py-2 px-4 hover:bg-black"
+                className="py-2 px-8 hover:bg-black"
                 onClick={() => {
                   setSelected(key as ComponentType);
                 }}
@@ -26,7 +27,12 @@ function App() {
           })}
         </div>
       </ScrollArea>
-      <div className="flex-grow">{components[selected].component}</div>
+      <div className="flex flex-grow flex-col p-8 max-w-[800px]">
+        <h1 className="text-2xl mb-4 font-bold">{selected}</h1>
+        <div className="flex flex-col w-full items-center border border-dotted border-border p-8">
+          {components[selected].component}
+        </div>
+      </div>
     </div>
   );
 }
