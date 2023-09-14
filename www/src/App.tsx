e268f402fc9c24a0abd73ffb8f4bbfc0380f83a9
dxu <xudavidb@gmail.com>
2023-09-14 14:26:57 -0700

    add docs and api changes

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -1,8 +1,12 @@
-import { ScrollArea, Toaster } from "wobbui";
+import { Badge, ScrollArea, Toaster } from "wobbui";
 
 import { useState } from "react";
 
 import components from "./mappings";
+
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { ExternalLink } from "lucide-react";
 
 type ComponentType = keyof typeof components;
 
@@ -27,10 +31,35 @@ function App() {
           })}
         </div>
       </ScrollArea>
-      <div className="flex flex-grow flex-col p-8 max-w-[800px]">
-        <h1 className="text-2xl mb-4 font-bold">{selected}</h1>
-        <div className="flex flex-col w-full items-center border border-dotted border-border p-8">
-          {components[selected].component}
+      <div className="flex flex-grow flex-col p-8 max-w-[800px] min-w-[1px]">
+        <div className="flex items-center mb-4 gap-4">
+          <h1 className="text-2xl font-bold">{selected}</h1>
+          <a
+            target="_blank"
+            href={`https://www.radix-ui.com/primitives/docs/components/${selected
+              .split(" ")
+              .join("-")
+              .toLowerCase()}`}
+          >
+            <Badge className="gap-1">
+              Radix API
+              <ExternalLink size={12} />
+            </Badge>
+          </a>
+        </div>
+        <div className="flex flex-col w-full gap-4">
+          <div className="flex flex-col w-full">
+            <h2 className="text-xs uppercase mb-2 font-bold">Preview</h2>
+            <div className="flex flex-col w-full items-center p-8  border border-dotted border-border">
+              {components[selected].component}
+            </div>
+          </div>
+          <div className="flex flex-col w-full">
+            <h2 className="text-xs uppercase font-bold">Code</h2>
+            <SyntaxHighlighter language="javascript" style={vscDarkPlus}>
+              {components[selected].raw}
+            </SyntaxHighlighter>
+          </div>
         </div>
       </div>
     </div>
