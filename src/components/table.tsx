fc0224f692fc4f8c93569a2fd39e55853dddcbe5
dxu <xudavidb@gmail.com>
2023-09-30 13:54:36 -0700

    remove table wrapper

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -8,13 +8,11 @@ const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
 >(({ className, ...props }, ref) => (
-  <div className="w-full overflow-auto">
-    <table
-      ref={ref}
-      className={cn("w-full caption-bottom text-sm", className)}
-      {...props}
-    />
-  </div>
+  <table
+    ref={ref}
+    className={cn("w-full caption-bottom text-sm", className)}
+    {...props}
+  />
 ));
 Table.displayName = "Table";
 
